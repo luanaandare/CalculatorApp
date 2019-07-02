0a6f6c840ecb39832b31a2fdea25b7bb8288afb5
luanaandare <luanaandare@hotmail.com>
2019-07-01 21:29:03 -0300

    testando

---
+++ app/calculator-app/src/App.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import './App.css';
+import resultadoComponent from './components/resultado';
+import tecladoComponent from "./components/teclado";
+
+class App extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            result: ""
+        }
+    }
+
+    onClick = button => {
+
+        if(button === "="){
+            this.calculate()
+        }
+
+        else if(button === "C"){
+            this.reset()
+        }
+        else if(button === "CE"){
+            this.backspace()
+        }
+
+        else {
+            this.setState({
+                result: this.state.result + button
+            })
+        }
+    };
+
+
+    calculate = () => {
+        var checkResult = ''
+        if(this.state.result.includes('--')){
+            checkResult = this.state.result.replace('--','+')
+        }
+
+        else {
+            checkResult = this.state.result
+        }
+
+        try {
+            this.setState({
+                // eslint-disable-next-line
+                result: (eval(checkResult) || "" ) + ""
+            })
+        } catch (e) {
+            this.setState({
+                result: "error"
+            })
+
+        }
+    };
+
+    reset = () => {
+        this.setState({
+            result: ""
+        })
+    };
+
+    backspace = () => {
+        this.setState({
+            result: this.state.result.slice(0, -1)
+        })
+    };
+
+    render() {
+        return (
+            <div>
+                <div className="calculator-body">
+                    <h1>Calculadora React</h1>
+                    <resultadoComponent result={this.state.result}/>
+                    <tecladoComponent onClick={this.onClick}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
+
+
---
+++ app/calculator-app/src/components/resultado.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class resultadoComponent extends Component {
+
+
+    render() {
+        let {result} = this.props;
+        return (
+            <div className="result">
+                <p>{result}</p>
+            </div>
+    )
+        ;
+    }
+}
+
+
+export default resultadoComponent;
